77bddb8fc9944ffc5ff5367c71cf25a65578cba7
arash sheydaei <arisheydaei@gmail.com>
2025-05-19 16:33:01 +0330

    Update build configuration and add CNAME

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/arashcoder.dev/',
+  base: './',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
+    emptyOutDir: true,
+    sourcemap: true
   }
 })
